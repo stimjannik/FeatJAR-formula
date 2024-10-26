5cf050e7520c8f812f137f9fe355c6b8e911b37f
stimjannik <tim.j.schmidt@web.de>
2024-10-27 00:33:32 +0200

    Update IncInteractionFinderOnlyWorkingConfigurations

--- src/main/java/de/featjar/formula/computation/interactionfinder/IncInteractionFinderOnlyWorkingConfigurations.java
+++ src/main/java/de/featjar/formula/computation/interactionfinder/IncInteractionFinderOnlyWorkingConfigurations.java
@@ -1,20 +1,27 @@
 package de.featjar.formula.computation.interactionfinder;
 
+import de.featjar.base.FeatJAR;
 import de.featjar.base.data.IntegerList;
+import de.featjar.base.data.LexicographicIterator;
+import de.featjar.formula.assignment.ABooleanAssignment;
 import de.featjar.formula.assignment.BooleanAssignment;
+import de.featjar.formula.assignment.BooleanClauseList;
 import de.featjar.formula.assignment.BooleanSolution;
 
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class IncInteractionFinderOnlyWorkingConfigurations extends AInteractionFinder {
     private ArrayList<Integer> errors = new ArrayList<>();
+    private static final double limitFactor = 10.0 / Math.log(2);
+
 
     @Override
     public List<BooleanAssignment> find(int tmax) {
-//        if (failingConfs.isEmpty()) {
-//            return null;
-//        }
+        if (failingConfs.isEmpty()) {
+            return null;
+        }
         verifyCounter = 0;
         lastMerge = null;
 
@@ -74,7 +81,7 @@ public class IncInteractionFinderOnlyWorkingConfigurations extends AInteractionF
         final List<int[]> result = lastI == -1 ? null : results[lastI];
         return isPotentialInteraction(result)
                 ? List.of(new BooleanAssignment(
-                        IntegerList.mergeInt(result.stream().collect(Collectors.toList()))))
+                IntegerList.mergeInt(result.stream().collect(Collectors.toList()))))
                 : null;
     }
 
@@ -88,6 +95,9 @@ public class IncInteractionFinderOnlyWorkingConfigurations extends AInteractionF
         if (curInteractionList == null) {
             return null;
         }
+
+        setConfigurationVerificationLimit((int) Math.ceil(limitFactor * Math.log(curInteractionList.size())));
+
 
         while (curInteractionList.size() > errors.size() //
                 && verifyCounter < configurationVerificationLimit) {
@@ -103,49 +113,88 @@ public class IncInteractionFinderOnlyWorkingConfigurations extends AInteractionF
             int diff = Math.abs(include.size() - exclude.size());
             int lastDiff = diff;
 
-            while (diff > 1) {
-                BooleanSolution config;
-                if (include.size() > exclude.size()) {
-                    config = updater.complete(null, exclude, include).orElse(null);
-                    if (config == null) {
-                        break;
+            loop:
+            while (verifyCounter < configurationVerificationLimit) {
+                while (diff > 1) {
+                    BooleanSolution config;
+                    if (include.size() > exclude.size()) {
+                        config = updater.complete(null, exclude, include).orElse(null);
+                        if (config == null) {
+                            break;
+                        }
+                        partitions = group(include, config);
+                        assert partitions.get(Boolean.FALSE) != null;
+                        assert partitions.get(Boolean.TRUE) != null;
+                        diff = Math.abs(
+                                (exclude.size() + partitions.get(Boolean.FALSE).size())
+                                        - partitions.get(Boolean.TRUE).size());
+                        if (diff >= lastDiff) {
+                            break;
+                        }
+                        exclude.addAll(partitions.get(Boolean.FALSE));
+                        include = partitions.get(Boolean.TRUE);
+                    } else {
+                        config = updater.complete(include, null, exclude).orElse(null);
+                        if (config == null) {
+                            break;
+                        }
+                        partitions = group(exclude, config);
+                        assert partitions.get(Boolean.FALSE) != null;
+                        assert partitions.get(Boolean.TRUE) != null;
+                        diff = Math.abs(
+                                (include.size() + partitions.get(Boolean.TRUE).size())
+                                        - partitions.get(Boolean.FALSE).size());
+                        if (diff >= lastDiff) {
+                            break;
+                        }
+                        include.addAll(partitions.get(Boolean.TRUE));
+                        exclude = partitions.get(Boolean.FALSE);
                     }
-                    partitions = group(include, config);
-                    assert partitions.get(Boolean.FALSE) != null;
-                    assert partitions.get(Boolean.TRUE) != null;
-                    diff = Math.abs(
-                            (exclude.size() + partitions.get(Boolean.FALSE).size())
-                                    - partitions.get(Boolean.TRUE).size());
-                    if (diff >= lastDiff) {
-                        break;
+                    if (!configurationPool.contains(config)) {
+                        lastDiff = diff;
+                        bestConfig = config;
+                    } else {
+                        bestConfig =
+                                updater.complete(null, null, null).orElse(null);
+                        partitions = group(curInteractionList, bestConfig);
+                        include = partitions.get(Boolean.TRUE);
+                        exclude = partitions.get(Boolean.FALSE);
+                        if (exclude == null) {
+                            exclude = new ArrayList<>();
+                        }
+                        if (include == null) {
+                            include = new ArrayList<>();
+                        }
+                        diff = Math.abs(include.size() - exclude.size());
+                        lastDiff = diff;
                     }
-                    exclude.addAll(partitions.get(Boolean.FALSE));
-                    include = partitions.get(Boolean.TRUE);
-                } else {
-                    config = updater.complete(include, null, exclude).orElse(null);
-                    if (config == null) {
-                        break;
-                    }
-                    partitions = group(exclude, config);
-                    assert partitions.get(Boolean.FALSE) != null;
-                    assert partitions.get(Boolean.TRUE) != null;
-                    diff = Math.abs(
-                            (include.size() + partitions.get(Boolean.TRUE).size())
-                                    - partitions.get(Boolean.FALSE).size());
-                    if (diff >= lastDiff) {
-                        break;
-                    }
-                    include.addAll(partitions.get(Boolean.TRUE));
-                    exclude = partitions.get(Boolean.FALSE);
                 }
-                lastDiff = diff;
-                bestConfig = config;
-            }
 
-            final boolean pass = verify(bestConfig);
-            curInteractionList = pass ? exclude : include;
-            if (lastMerge != null && pass == bestConfig.containsAll(lastMerge)) {
-                lastMerge = null;
+                final boolean pass = verify(bestConfig);
+                if (pass) {
+                    curInteractionList = exclude;
+                    if (lastMerge != null && pass == bestConfig.containsAll(lastMerge)) {
+                        lastMerge = null;
+                    }
+                    break loop;
+                } else {
+                    bestConfig =
+                            updater.complete(null, null, null).orElse(null);
+                    partitions = group(curInteractionList, bestConfig);
+                    include = partitions.get(Boolean.TRUE);
+                    exclude = partitions.get(Boolean.FALSE);
+                    if (exclude == null) {
+                        exclude = new ArrayList<>();
+                    }
+                    if (include == null) {
+                        include = new ArrayList<>();
+                    }
+                    diff = Math.abs(include.size() - exclude.size());
+                    lastDiff = diff;
+                }
+            }
+            if(configurationVerificationLimit == verifyCounter){
+                throw new RuntimeException();
             }
         }
 
@@ -157,8 +206,26 @@ public class IncInteractionFinderOnlyWorkingConfigurations extends AInteractionF
         }
     }
 
+    public void addConfigurations(List<? extends ABooleanAssignment> configurations) {
+        configurations.stream().map(ABooleanAssignment::toSolution).forEach(this::initial_verify);
+    }
+
+    protected boolean initial_verify(BooleanSolution solution) {
+        verifyCounter++;
+        if (verifier.test(solution) == 0) {
+            succeedingConfs.add(solution);
+            return true;
+        } else {
+            failingConfs.add(solution);
+            return false;
+        }
+    }
+
     protected boolean verify(BooleanSolution solution) {
         verifyCounter++;
+        if (!configurationPool.contains(solution)) {
+            configurationPool.add(solution);
+        }
         final int error = verifier.test(solution);
         if (error == 0) {
             succeedingConfs.add(solution);
